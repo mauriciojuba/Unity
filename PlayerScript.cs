12e388c23418c69fd1b0254436e8df9a336052df
mauriciojuba <mauriciojuba@outlook.com>
2016-03-19 16:00:26 -0300

    Added files via upload
    Point and Click Player Movement

---
+++ PlayerScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public string Name = "Juba";
+    public float health = 100f;
+    public float damange = 30f;
+    public float range;
+
+    private NavMeshAgent agent;
+    //public Transform target;
+    public Camera myCamera;
+    public GameObject marker;
+
+    
+    void Awake () {
+        marker = GameObject.Find("Target");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+	void Update () {
+        Move();
+    }
+
+    void Attack()
+    {
+
+    }
+    void Move()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            marker.SetActive(true);
+            Ray ray = myCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 200))
+            {
+                marker.transform.position = hit.point;
+            }
+        }
+        if(Vector3.Distance(marker.transform.position, transform.position) <= 1)
+        {
+            marker.SetActive(false);
+        }
+
+        agent.SetDestination(marker.transform.position);
+    }
+}
