8fe8dda41aad63630ef51e287d9357ced7c149d1
Mauricio Juba <mauriciojuba@outlook.com>
2016-03-25 17:39:24 -0300

    Highlights Objects OnMouseOver()

---
+++ InteractiveObjectsShaderHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveObjectsShaderHandler : MonoBehaviour {
+
+    //Variavel que guardará o shader inicial do objeto
+    private string InitialShader;
+
+    bool gotShader = false;
+
+    //Quando o mouse está em cima do objeto
+    void OnMouseOver()
+    {
+        //se ele já guardou o shader inativo, então ele pode trocar para o shader de highlight
+        if (gotShader) GetComponent<Renderer>().material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+        //se não
+        else
+        {
+            //se ele ainda não guardou o nome do shader inativo, então ele deve guardar.
+            InitialShader = GetComponent<Renderer>().material.shader.name;
+            //avisa que o nome já foi guardado
+            gotShader = true;
+        }
+    }
+
+    //Mouse fora do objeto
+    void OnMouseExit()
+    {
+        //volta para o shader inicial
+        GetComponent<Renderer>().material.shader = Shader.Find("" + InitialShader);
+    }
+}
